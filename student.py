3ee8171cad0804307c0e38b73c2bc4a29fd34d4d
allenzhai <azhai@calpoly.edu>
2020-01-06 08:41:13 -0800

    init

---
+++ student.py
@@ -0,0 +1,16 @@
+
+
+class Student:;
+    def __init__(self, StLastName, StFirstName, Grade, Classroom, Bus, GPA, TLastName, TFirstName):
+        self.StLastName = StLastName
+        self.StFirstName = StFirstName
+        self.Grade = Grade
+        self.Classroom = Classroom
+        self.Bus = Bus
+        self.GPA = GPA
+        self.TLastName = TLastName
+        self.TFirstName = TFirstName
+
+
+def parseStudents():
+    pass
