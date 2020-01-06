bf95e251182bc563d1b0b0bb34e63f90c2654b2b
allenzhai <azhai@calpoly.edu>
2020-01-06 09:26:00 -0800

    parsed text into student (class) list

--- student.py
+++ student.py
@@ -11,12 +11,20 @@ class Student:
         self.TLastName = TLastName
         self.TFirstName = TFirstName
 
+    def __repr__(self):
+        return "Last Name: {}\nFirst Name: {}\nGrade: {}\nClassroom: {}\nBus: {}\nGPA: {}\nTeacher Last Name: {}\nTeacher First Name: {}\n".format(self.StLastName, self.StFirstName, self.Grade, self.Classroom, self.Bus, self.GPA, self.TLastName, self.TFirstName)
 
 def parseStudents(filename):
+    Students = []
     with open(filename, "r") as studentsFile:
         for line in studentsFile:
-            info = [line.strip("\n").split(",")]
-            print(info)
+            info = line.strip("\n").split(",")
+            s = Student(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7])
+            Students.append(s)
+            print(s)
+
+
+
 
 
     pass
