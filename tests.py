17e90e6e68625fd8cc426b5def58e3668c0ed0a6
stevengandham <stevengandham@gmail.com>
2020-01-08 09:04:34 -0800

    added 'output' return to functions.py and added tests.py framework

---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+import functions
+import student
+import io
+import sys
+
+
+class Test(unittest.TestCase):
+
+
+    def test1(self):
+        pass
+    def test2(self):
+        pass
+    def test3(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
