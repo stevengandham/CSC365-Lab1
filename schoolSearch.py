a28ab3058611f821f4836e4de6b8f38bf866f793
Wei Lin <wlin19@unix5.csc.calpoly.edu>
2020-01-08 08:21:13 -0800

    name change

--- schoolSearch.py
+++ schoolSearch.py
@@ -1,113 +1,35 @@
-from decimal import Decimal
-#  S[tudent]: <lastname>
-def R4(students, user_in):
-    """ Search the contents of the students.txt file for the entry (or entries) for students with
-    the given last name. For each entry found, print the last name, first name, grade and classroom
-    assignment for each student found and the name of their teacher (last and first name). """
-    for student in students:
-        if user_in == student.StLastName:
-            print(student.StLastName + "," + student.StFirstName + "," + student.Grade + 
-            ","  + student.Classroom + "," + student.TLastName + "," +  student.StFirstName)
+import functions
+import student
+import sys
+import os
 
-# S[tudent]: <lastname> B[us]
-def R5(students, user_in):
-    """ Search the contents of the students.txt file for the entry (or entries) for students with
-        the given last name. For each entry found, print the last name, first name and the bus
-        route the student takes. """
-    for student in students:
-        if user_in == student.StLastName:
-            print(student.StLastName + "," +student.StFirstName + "," + student.Bus)
+def main():
+    students = student.parseStudents()
+    not_quit = True
 
-# T[eacher]: <lastname>
-def R6(students, user_in):
-   """ Search the contents of the students.txt file for the entries where the last name of the
-       teacher matches the name provided in the instruction. For each entry found, print the
-       last and the first name of the student. """
-   for student in students:
-      if user_in == student.TLastName:
-         print(student.StLastName + "," + student.StFirstName)
-
-# G[rade]: <Number>
-def R7(students, user_in):
-   """ Search the contents of the students.txt file for the entries where the student's grade
-       matches the number provided in the instruction. For each entry, output the name (last and first)
-       of the student. """
-   for student in students:
-      if user_in == student.Grade:
-         print(student.StLastName + "," + student.StFirstName)
-
-# B[us]: <Number>
-def R8(students, user_in):
-   """ Search the contents of the students.txt file for the entries where the bus route number
-       matches the number provided in the instruction. For each such entry, output the first and
-       the last name of the student and their grade and classroom."""
-   for student in students:
-      if user_in == student.Bus:
-         print(student.StLastName + "," + student.StFirstName + "," +
-               student.Grade + "," + student.Classroom)
-
-#G[rade]: <Number> H[igh]
-#                   or
-#G[rade]: <Number> L[ow]
-def R9(students, user_in):
-    """ Search the contents of the students.txt file for the entries where the student's grade
-        matches the number provided in the instruction.
-
-        If the H[igh] keyword is used in the command, find the entry in the students.txt file
-        for the given grade with the highest GPA. Report the contents of this entry (name of the
-        student, GPA, teacher, bus route).
-
-        If the L[ow] keyword is used in the command, find the entry in the students.txt file
-        for the given grade with the lowest GPA. Report the contents of this entry (name of the
-        student, GPA, teacher, bus route)."""
-    user_in = user_in.split();
-    studentList = [student for student in students if user_in[0] == student.Grade]
-
-    if user_in[1].strip('igh') == "H":
-        maxGPA = max(student.GPA for student in studentList)
-        maxGPAStudent = [student for student in studentList if student.GPA == maxGPA][0]
-        print(maxGPAStudent.StLastName + "," + maxGPAStudent.StFirstName + "," +
-              maxGPAStudent.GPA + "," + maxGPAStudent.TLastName + "," +
-              maxGPAStudent.TFirstName + "," + maxGPAStudent.Bus)
-    if user_in[1].strip('ow') == "L":
-        minGPA = min(student.GPA for student in studentList)
-        minGPAStudent = [student for student in studentList if student.GPA == minGPA][0]
-        print(minGPAStudent.StLastName + "," + minGPAStudent.StFirstName + "," +
-            minGPAStudent.GPA + "," + minGPAStudent.TLastName + "," +
-            minGPAStudent.TFirstName + "," +  minGPAStudent.Bus)
+    while not_quit:
+        user_in = input().split(" ")
+        if user_in[0][0] == 'S':
+            if len(user_in) == 3 and user_in[2][0] == 'B':
+                schoolSearch.R5(students, user_in[1])
+            else:
+                schoolSearch.R4(students, user_in[1])
+        elif user_in[0][0] == 'T':
+            schoolSearch.R6(students, user_in[1])
+        elif user_in[0][0] == 'B':
+            schoolSearch.R8(students, user_in[1])
+        elif user_in[0][0] == 'G':
+            if len(user_in) == 3:
+                schoolSearch.R9(students, " ".join(user_in[1:]))
+            else:
+                schoolSearch.R7(students, user_in[1])
+        elif user_in[0][0] == 'A':
+            schoolSearch.R10(students, user_in[1])
+        elif user_in[0][0] == 'I':
+            schoolSearch.R11(students)
+        if user_in[0][0] == 'Q':
+            not_quit = False
 
 
-# A[verage]: <Number>
-def R10(students, user_in):
-    """ Search the contents of the students.txt file for the entries where the student's grade
-        matches the number provided in the instruction.Compute the average GPA score for
-        the entries found. Output the grade level (the number provided in command) and the
-        average GPA score computed."""
-    studentList = [student for student in students if user_in == student.Grade]
-    
-    GPAList = [Decimal(student.GPA) for student in studentList]
-    GPAAvg = sum(GPAList)/len(GPAList);
-
-    print(user_in + "," + str(round(GPAAvg, 2)))
-
-
-# I[nfo]
-def R11(students):
-    """ For each grade (from 0 to 6) compute the total number of students in that grade.
-        Report the number of students in each grade in the format
-
-            <Grade>: <Number of Students>
-
-        sorted in ascending order by grade."""
-
-    gradeList = [student.Grade for student in students]
-    classCount = []
-    for i in range(0,7):
-        classCount.append(str(gradeList.count(str(i))))
-    print("Grade 0: " + classCount[0] +
-          "\nGrade 1: " + classCount[1] +
-          "\nGrade 2: " + classCount[2] +
-          "\nGrade 3: " + classCount[3] +
-          "\nGrade 4: " + classCount[4] +
-          "\nGrade 5: " + classCount[5] +
-          "\nGrade 6: " + classCount[6])
+if __name__ == '__main__':
+    main()
