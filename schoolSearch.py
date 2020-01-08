f6cde33e679ab92e05edd485f3852ac61f8670a4
Wei Lin <wlin19@unix5.csc.calpoly.edu>
2020-01-08 08:59:40 -0800

    added error handling

--- schoolSearch.py
+++ schoolSearch.py
@@ -1,5 +1,5 @@
 """ ALLEN ZHAI, WEI LIN, STEVEN GANDHAM """
-import functions
+from functions import *
 import student
 import sys
 import os
@@ -16,24 +16,44 @@ def main():
         user_in = input().split(" ")
         if user_in[0][0] == 'S':
             if len(user_in) == 3 and user_in[2][0] == 'B':
-                schoolSearch.R5(students, user_in[1])
+                R5(students, user_in[1])
+            elif len(user_in) == 2 and user_in[2][0] == 'B':
+                R4(students, user_in[1])
             else:
-                schoolSearch.R4(students, user_in[1])
+                print("Invalid number of arguments")
+                print("Usage: S[tudent]: <lastname> [B[us]]")
         elif user_in[0][0] == 'T':
-            schoolSearch.R6(students, user_in[1])
+            if len(user_in) == 2:
+               R6(students, user_in[1])
+            else:
+                print("Invalid number of arguments")
+                print("Usage: T[eacher]: <lastname>")
         elif user_in[0][0] == 'B':
-            schoolSearch.R8(students, user_in[1])
+            if len(user_in) == 2:
+               R8(students, user_in[1])
+            else:
+                print("Invalid number of arguments")
+                print("Usage: B[us]: <number>")
         elif user_in[0][0] == 'G':
             if len(user_in) == 3:
-                schoolSearch.R9(students, " ".join(user_in[1:]))
+                R9(students, " ".join(user_in[1:]))
+            elif len(user_in) == 2:
+                R7(students, user_in[1])
             else:
-                schoolSearch.R7(students, user_in[1])
+                print("Invalid number of arguments")
+                print("Usage: G[rade]: <number> [H[igh]|L[ow]]")
         elif user_in[0][0] == 'A':
-            schoolSearch.R10(students, user_in[1])
-        elif user_in[0][0] == 'I':
-            schoolSearch.R11(students)
-        if user_in[0][0] == 'Q':
+            if len(user_in) == 2:
+               R10(students, user_in[1])
+            else:
+                print("Invalid number of arguments")
+                print("Usage: A[verage]: <number>")
+        elif len(user_in) == 1 and user_in[0][0] == 'I': 
+            R11(students)
+        elif user_in[0][0] == 'Q':
             not_quit = False
+        else:
+            print("Invalid command")
 
 
 if __name__ == '__main__':
