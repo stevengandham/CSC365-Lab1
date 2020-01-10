3fdfcc56d5014cd2b3e29b9dd70da0d43b6b2b72
allenzhai <azhai@calpoly.edu>
2020-01-10 08:20:30 -0800

    fixed 0 bug

--- tests.py
+++ tests.py
@@ -59,7 +59,7 @@ class Test(unittest.TestCase):
     def testR10(self):
         students = student.parseStudents()
         self.assertEqual(R10(students, "3"), [["3,3.05"]])
-        self.assertEqual(R10(students, "0"), [["0,0.0"]])
+        self.assertEqual(R10(students, "0"), [["0,0"]])
     def testR11(self):
         students = student.parseStudents()
         self.assertEqual(R11(students), [['Grade 0', '0'],
